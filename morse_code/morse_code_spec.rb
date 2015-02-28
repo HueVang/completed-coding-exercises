8db5ec8a22578566e4a5b1ccf19d9f998a7fbd5d
Hue Vang <huevang100@gmail.com>
2015-02-27 23:31:44 -0600

    Create morse_code_spec.rb

---
+++ morse_code/morse_code_spec.rb
@@ -0,0 +1,21 @@
+require './morse_code1'
+require 'rspec'
+
+# Build a function, `morse_encode(str)` that takes in a string (no
+# numbers or punctuation) and outputs the morse code for it. See
+# http://en.wikipedia.org/wiki/Morse_code. Put two spaces between
+# words and one space between letters.
+
+describe "#morse_encode" do
+  it "should do a simple letter" do
+    morse_encode("q").should == "--.-"
+  end
+
+  it "should handle a small word" do
+    morse_encode("cat").should == "-.-. .- -"
+  end
+
+  it "should handle a phrase" do
+    morse_encode("cat in hat").should == "-.-. .- -  .. -.  .... .- -"
+  end
+end
