6520e3a75c197520f85bd5669c6241e12fbf0fed
Hue Vang <huevang100@gmail.com>
2015-02-27 23:32:20 -0600

    Update and rename morse_code to morse_code/morse_code1.rb

---
+++ morse_code/morse_code1.rb
@@ -0,0 +1,40 @@
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+
+def morse_encode(str)
+	words = str.split(" ")
+	result = words.map { |word| morse_encode_word(word) }
+	return result.join("  ")
+end
+
+def morse_encode_word(word)
+	letters = word.split("")
+	code = letters.map {|l| MORSE_CODE[l]}
+	code.join(" ")
+end
